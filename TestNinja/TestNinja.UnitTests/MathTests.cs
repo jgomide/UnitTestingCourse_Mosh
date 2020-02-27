99f174a46e917e41a934c2a591e24b20f7020ea3
Gomide <jefersongomide@gmail.com>
2020-02-27 13:52:08 +1100

    Fundamentals of Unit Testing (40m)

---
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+using TestNinja.Fundamentals;
+using NUnit.Framework;
+using System.IO;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        private Math _math;
+
+        //Setup
+        //TearDown
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+
+        [Test]
+        //[Ignore("Because I wanted to!")]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            //var math = new Math();
+
+            //Debug.Assert(_math != null, nameof(_math) + " != null");
+            var result = _math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        [TestCase(2, 1,2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(1, 1, 1)]
+        public void Max_WhenCalled_ReturnTheGreaterArgument(int a, int b, int expectedResult)
+        {
+            //var math = new Math();
+
+            var result = _math.Max(a, b);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ TestNinja/TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    //[TestClass]
+    [TestFixture]
+    public class ReservationTests
+    {
+        //[TestMethod]
+        [Test]
+        public void CanBeCancelledBy_AdminCancelling_ReturnsTrue()
+        {
+            //Arrange
+            var reservation = new Reservation();
+            //Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+            //Assert
+            Assert.IsTrue(result);
+            Assert.That(result, Is.True);
+            Assert.That(result == true);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_SameUserCancelling_ReturnTrue()
+        {
+            var user = new User();
+            var reservation = new Reservation { MadeBy = user };
+
+            var result = reservation.CanBeCancelledBy(user);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test] 
+        public void CanBeCancelledBy_AnotherUserCancelling_ReturnFalse(){
+            var reservation = new Reservation { MadeBy = new User() };
+
+            var result = reservation.CanBeCancelledBy(new User());
+
+            Assert.IsFalse(result);
+        }
+    }
+}
